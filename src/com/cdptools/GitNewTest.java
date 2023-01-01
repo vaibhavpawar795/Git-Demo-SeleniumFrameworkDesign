b506214df323879be4b3ff3e26c8cccbf9a01f42
vaibhavpawar795 <vaibhavpawar795@gmail.com>
2023-01-01 17:04:07 +0530

    final commit by European guy present here at project

---
+++ src/com/cdptools/GitNewTest.java
@@ -0,0 +1,29 @@
+package com.cdptools;
+
+public class GitNewTest {
+	public void testschanges() {
+		System.out.println("Browser automation");
+
+		System.out.println("Browser automation");
+
+		System.out.println("WebAutomation");
+		System.out.println("WebAutomation");
+	}
+	
+	public void tests() {
+		System.out.println("Browser automation");
+
+		System.out.println("Browser automation");
+
+		System.out.println("WebAutomation");
+		System.out.println("WebAutomation");
+	}
+	public void testsStart() {
+		System.out.println("Browser automation");
+
+		System.out.println("Browser automation");
+
+		System.out.println("WebAutomation");
+		System.out.println("WebAutomation");
+	}
+}
