9134297be2023d56c84a34dbf8c42e4783e2f2d9
vaibhavpawar795 <vaibhavpawar795@gmail.com>
2023-01-01 14:54:54 +0530

    This is commit from Asian Time zone

--- src/BasicAuthentication.java
+++ src/BasicAuthentication.java
@@ -10,13 +10,25 @@ import java.util.function.Predicate;
 public class BasicAuthentication {
 
 	public static void main(String[] args) {
-		//System.setProperty("webdriver.chrome.driver", "/Users/rahulshetty/Documents/chromedriver");
-		WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver", "/Users/rahulshetty/Documents/chromedriver");
+		
 		ChromeDriver driver = new ChromeDriver();
 		//predicate, consumer
 		Predicate<URI> uriPredicate =uri ->  uri.getHost().contains("httpbin.org");
 						
 		((HasAuthentication)driver).register(uriPredicate,UsernameAndPassword.of("foo", "bar"));
-		driver.get("http://httpbin.org/basic-auth/foo/bar");		
+		driver.get("http://httpbin.org/basic-auth/foo/bar");
+		
+		
+		System.out.println("this is git update");
+		
+		
+		System.out.println("this is git updatebb for this purposes");
+		
+		System.out.println("this is git updatebb for this purposes");
+		
+		System.out.println("this is git update");
+		
+		
 	}   
 }
